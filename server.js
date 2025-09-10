05d0829e3ce4b06ca8fe025c37fbbeb48e8249c4
parthhhh2802 <parthsolanki2802@gmail.com>
2025-09-10 22:35:17 +0530

    server.js across platform 10th sep

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
-import {createServer} from 'http';
-import {isEven} from './isEven.js';
-import {URL} from 'url';
+import { createServer } from 'http';
+import { isEven } from './isEven.js';
+import { URL } from 'url';
 
-createServer((req,res) => {
+createServer((req, res) => {
     const reqUrl = new URL(req.url, `http://${req.headers.host}`);
     const numberParam = reqUrl.searchParams.get('number');
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.writeHead(200, {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*'
+    });
+
+
+
     if (numberParam === null) {
         res.end('Please provide a number query parameter, e.g., ?number=4');
         return;
-    } else {
-        const number = parseInt(numberParam, 10);
+    }
+
+    const number = parseInt(numberParam, 10);
+
+    if (isNaN(number)) {
+        res.end('Error: Please enter a valid integer.');
+        return;
+    }
+
+    try {
         res.end(isEven(number) ? `${number} is even` : `${number} is odd`);
+    } catch (err) {
+        res.end('Error: ' + err.message);
     }
 }).listen(8080, () => {
     console.log('Server is listening on port 8080');
 });
-
-
